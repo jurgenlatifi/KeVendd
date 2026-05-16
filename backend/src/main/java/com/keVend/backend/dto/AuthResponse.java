084bf777993c32080f3de23f48340562ca17a8ad
jurgenlatifi <128168472+jurgenlatifi@users.noreply.github.com>
2026-05-16 17:22:23 +0200

    Fix: Add missing fields to AuthResponse DTO

--- backend/src/main/java/com/keVend/backend/dto/AuthResponse.java
+++ backend/src/main/java/com/keVend/backend/dto/AuthResponse.java
@@ -2,12 +2,17 @@ package com.keVend.backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
     private String accessToken;
     private String refreshToken;
     private String role;
     private Long userId;
+    private String name;
+    private String surname;
+    private String phone;
 }
